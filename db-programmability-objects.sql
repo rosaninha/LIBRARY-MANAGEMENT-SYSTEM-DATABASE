b758bb82b296bb048111d89376f7d5f21522e096
Rosana <65780925+rosaninha@users.noreply.github.com>
2020-07-31 20:47:01 -0300

    Update db-programmability-objects.sql

--- db-programmability-objects.sql
+++ db-programmability-objects.sql
@@ -3,13 +3,18 @@ SELECT LastName, Email
 FROM User.UserDetails
 WHERE UserTypeId = '1'; 
 
-CREATE VIEW EnglishBook AS
-SELECT BookTitle, Author 
+CREATE VIEW PortugueseBook AS
+SELECT BookTitle, Author, ShelfNumber, AisleNumber 
 FROM Book.BookBiding
 WHERE Language = 'Portuguese'; 
 
 
 CREATE VIEW EnglishBook AS
-SELECT BookTitle, Author 
+SELECT BookTitle, Author, ShelfNumber, AisleNumber 
 FROM Book.BookBiding
 WHERE CategoryName = 'Romance'; 
+
+CREATE VIEW WaitingList AS
+SELECT BookId, BorrowerdTo,
+FROM Book.Borrower
+WHERE WaitingList = '1'; 
