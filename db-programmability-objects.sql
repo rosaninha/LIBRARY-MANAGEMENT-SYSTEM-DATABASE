b9136e74a98030ccb89113f3cf2d638f13012dd8
Rosana <65780925+rosaninha@users.noreply.github.com>
2020-08-04 20:23:01 -0300

    Update db-programmability-objects.sql

--- db-programmability-objects.sql
+++ db-programmability-objects.sql
@@ -18,3 +18,21 @@ CREATE VIEW WaitingList AS
 SELECT BookId, BorrowerdTo,
 FROM Book.Borrower
 WHERE WaitingList = '1'; 
+
+CREATE PROCEDURES User.UserDetails_INSERT
+  @UserId Int
+  @UserTypeId Int,
+  @FirstName nVarchar (50),
+  @MiddleName nVarchar (50),
+  @LastName nVarchar (50),
+  @Sex Varchar (10),
+  @DateBirth Date,
+  @IdentificationType Varchar (10),
+  @IdentificationNumber Varchar (10),
+  @Email Varchar (100),
+  @ContactNumber Varchar (10),
+  @ContactNumber2 Varchar (10),
+  @AddressLine1 Varchar (100),
+  @AddressLine2 Varchar (100)
+ AS
+  INSERT INTO User.UserDetails (
